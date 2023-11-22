bfc311c2e2f36cf78c5c2c33c91d9e26adb3fbfe
siilviia22 <147420135+siilviia22@users.noreply.github.com>
2023-11-22 22:39:22 +0100

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php
+// Simulació d'autenticació
+$usuaris_validats = array(
+    "usuari1" => "NomUsuari1",
+    "usuari2" => "NomUsuari2"
+);
+
+// Comprovació d'autenticació
+$usuari_actual = $_COOKIE['usuari'] ?? '';
+
+// Redirecció a la pàgina d'error si l'usuari no està validat
+if (!array_key_exists($usuari_actual, $usuaris_validats)) {
+    header("error.php");
+    exit;
+}
+?>
---
+++ signin.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+//connexió a la BBDD
+$conn = new mysqli($servername, $username, $password, $dbname); 
+
+//consulta
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
